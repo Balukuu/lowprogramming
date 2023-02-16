72fc6cdb7871738c3c51f4b59e77f4b614803390
Edgar <edgarbaluku@gmail.com>
2023-02-16 09:51:17 -0800

    fixed 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-//The main method
-int main(void)
-{
-	//using puts for the print
-	puts("\"Programming is like building a multilingual puzzle");
-	//returning a value 0
-	return (0);
+
+int main() {
+  puts("Programming is like building a multilingual puzzle\n");
+  return 0;
 }
+
