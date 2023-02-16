dd06e09d36364313b0dd8cbbf110373f97c3c58c
Edgar <edgarbaluku@gmail.com>
2023-02-16 09:45:36 -0800

    task 1,2and 3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//The main method
+int main(void)
+{
+	//using puts for the print
+	puts("\"Programming is like building a multilingual puzzle");
+	//returning a value 0
+	return (0);
+}
