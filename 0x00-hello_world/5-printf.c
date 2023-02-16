6a469457a1c5ca545296cf0e7f1b83720adf89c4
Edgar <edgarbaluku@gmail.com>
2023-02-16 10:05:24 -0800

    fixing betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints a piece of art with proper grammar
- *
- * Return: 0 (Success)
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
  */
+
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
