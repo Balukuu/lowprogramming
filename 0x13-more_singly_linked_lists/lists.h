2ee516e6e55579dce4b23c738c79518abde063df
Edgar Baluku <edgarbaluku@gmail.com>
2023-04-04 08:32:51 +0300

    updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,19 +2,19 @@
 #define LISTS_H
 
 /*
- * This header file defines a structure for a singly linked list node,
- * as well as function prototypes for various linked list manipulation functions.
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
  */
 
-#include <stdlib.h>  // Include the standard library header for malloc() and free().
+#include <stdlib.h>
 
 /**
- * struct listint_s - A singly linked list node structure for holding an integer value.
- * @n: The integer value stored in the node.
- * @next: A pointer to the next node in the list.
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: This structure defines a singly linked list node with an integer value
- *              and a pointer to the next node in the list.
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -22,9 +22,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Function prototypes for linked list manipulation */
-size_t print_listint(const listint_t *head);
-size_t listint_len(const listint_t *head);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
@@ -32,12 +31,11 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **head);
+size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
-
